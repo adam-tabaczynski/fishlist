13a3c1f769fbc1f09ca41d71af3f2f960fe82bdc
Adam Tabaczyński <adam.tabaczynski96@gmail.com>
2024-04-06 11:47:11 +0200

    Add Fish model, its database migration and register "Fishes" app in Django config

--- fishlist/fishes/models.py
+++ fishlist/fishes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Fish(models.Model):
+    length = models.IntegerField()
+    weight = models.IntegerField()
+    species = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return f"Fish, length: {self.length}, weight: {self.weight}, species: {self.species}"
